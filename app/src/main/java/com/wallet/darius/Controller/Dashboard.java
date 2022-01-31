d516d4117ef4dc504d46ea8d8d687ec0718effa3
SunWPS <wongsakorn.pi@ku.th>
2022-01-31 15:39:46 +0700

    add menu

--- app/src/main/java/com/wallet/darius/Controller/Dashboard.java
+++ app/src/main/java/com/wallet/darius/Controller/Dashboard.java
@@ -1,27 +1,99 @@
 package com.wallet.darius.Controller;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnSuccessListener;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.wallet.darius.R;
 
-import jnr.ffi.annotations.In;
+public class Dashboard extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-public class Dashboard extends AppCompatActivity {
-
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolBar;
+    TextView menuEmail;
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        auth = FirebaseAuth.getInstance();
+
+
+        
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolBar = findViewById(R.id.tool_bar);
+
+        menuSetUp();
+
+    }
+
+    public void menuSetUp() {
+
+        // set up header
+        View header = navigationView.getHeaderView(0);
+        menuEmail = header.findViewById(R.id.menu_email);
+        menuEmail.setText(auth.getCurrentUser().getEmail());
+
+        // toolbar
+        setSupportActionBar(toolBar);
+        getSupportActionBar().setTitle(null);
+        navigationView.setItemIconTintList(null);
+
+        // Navigation Menu
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,
+                drawerLayout,
+                toolBar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.getDrawerArrowDrawable().setColor(getResources().getColor(R.color.purple_200));
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+        navigationView.setCheckedItem(R.id.nav_change_pass);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.nav_change_pass:
+                break;
+            case R.id.nav_logout:
+                auth.signOut();
+                startActivity(new Intent(Dashboard.this, LoginScreen.class));
+                finish();
+                break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
